d3fdfe02674f0ebae7214fb89bf83b627faf211f
Anca <ancapantilie@gmail.com>
2018-05-03 14:15:41 +0300

    masonry archive

--- js/masonry-call.js
+++ js/masonry-call.js
@@ -8,11 +8,21 @@
 
 
         $( '.blog .hestia-blogs .blog-posts-wrap .card-blog' ).addClass( 'col-md-6 col-xs-12' );
+        $( '.archive .hestia-blogs .archive-post-wrap .card-blog' ).addClass( 'col-md-6 col-xs-12' );
         $( window ).load(
             function() {
                 if ( $( '.blog .hestia-blogs .blog-posts-wrap' ).length > 0 ) {
 
                     $( '.blog .hestia-blogs .blog-posts-wrap' ).masonry(
+                        {
+                            itemSelector: '.card-blog'
+                        }
+                    );
+                }
+
+                if ($( '.archive .hestia-blogs .archive-post-wrap' ).length > 0 ){
+
+                    $( '.archive .hestia-blogs .archive-post-wrap' ).masonry (
                         {
                             itemSelector: '.card-blog'
                         }
